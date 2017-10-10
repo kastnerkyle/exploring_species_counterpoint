54a9913e30b4c163ddeff2a883a4ea041f49730b
Kyle Kastner <kastnerkyle@gmail.com>
2017-10-10 15:30:02 -0400

    partial completion of cambiata rules

--- utils.py
+++ utils.py
@@ -1382,7 +1382,6 @@ def normalize_parts_with_durations(parts, durations):
                 is_event = True
                 which_events.append(n)
 
-
         if is_event:
             for n in range(len(parts)):
                 tt = round(time - prev_event_time, 4)
@@ -1525,6 +1524,9 @@ def rules_from_midi(parts, durations, key_signature):
                                           last_interval, last_motion, last_notes))
             i += 1
         all_rulesets.append(this_ruleset)
+    assert len(all_rulesets[0]) == len(full_intervals[0])
+    for ar in all_rulesets:
+        assert len(ar) == len(all_rulesets[0])
     return all_rulesets
 
 # previous movement, previous interval, previous notes
@@ -1854,7 +1856,7 @@ def parallel_rule(parts, durations, key_signature, time_signature, mode, timings
         dn1 = np.diff(np.array(notes_to_midi([[tn1, ln1]])[0]))
         note_sets = [[ln0, tn0], [ln1, tn1]]
         if li == "M10" or li == "m10":
-            if ti == "P8" and timings[0][idx] == "D":
+            if ti == "P8" and timings[0][idx] == 0.:
                 # battuta octave
                 returns.append((False, "parallel_rule: FALSE, battuta octave {}->{} disallowed on first beat".format(li, ti)))
                 continue
@@ -1905,12 +1907,40 @@ def beat_parallel_rule(parts, durations, key_signature, time_signature, mode, ti
             pm, pi, pn = pthis.split(":")
             if pi in ["P8", "P5"] and pi == ti:
                 # check beats - use the 0th voice?
-                if "D" == timings[0][idx] and "D" == timings[0][idx - 2] and abs(inverse_intervals_map[li]) < 5:
+                if 0. == timings[0][idx] and 0. == timings[0][idx - 2] and abs(inverse_intervals_map[li]) < 5:
                     returns.append((False, "beat_parallel_rule: FALSE, previous downbeat had parallel perfect interval {}".format(pi)))
                     continue
             returns.append((True, "beat_parallel_rule: TRUE, no beat parallel move"))
         else:
             returns.append((True, "beat_parallel_rule: TRUE, no beat parallel move"))
+    return returns
+
+
+def bar_consonance_rule(parts, durations, key_signature, time_signature, mode, timings, ignore_voices):
+    # ignore voices not used
+    rules = rules_from_midi(parts, durations, key_signature)
+    rules = rules[0]
+    key = key_signature_inv_map[key_signature]
+    returns = []
+    assert all([len(timings[i]) == len(timings[0]) for i in range(len(timings))])
+    for idx, rule in enumerate(rules):
+        last, this = rsp(rule)
+        tm, ti, tn = this.split(":")
+        tn0, tn1 = tn.split(",")
+
+        timing_i = timings[0][idx]
+        for n in range(len(timings)):
+            assert timings[n][idx] == timing_i
+
+        if timing_i == 0.:
+            if ti in harmonic_intervals or ti in neg_harmonic_intervals:
+                returns.append((True, "bar_consonance_rule: TRUE, harmonic interval {} allowed on downbeat".format(ti)))
+            else:
+                returns.append((False, "bar_consonance_rule: FALSE, non-consonant interval {} disallowed on downbeat".format(ti)))
+        elif timing_i != 0.:
+            returns.append((None, "bar_consonance_rule: NONE, rule not applicable on beat {}".format(timing_i)))
+        else:
+            raise ValueError("bar_consonance_rule: shouldn't get here")
     return returns
 
 
@@ -1930,12 +1960,9 @@ def passing_tone_rule(parts, durations, key_signature, time_signature, mode, tim
         for n in range(len(timings)):
             assert timings[n][idx] == timing_i
 
-        if timing_i == "D":
-            if ti in harmonic_intervals or ti in neg_harmonic_intervals:
-                returns.append((True, "passing_tone_rule: TRUE, harmonic interval {} allowed on downbeat".format(ti)))
-            else:
-                returns.append((False, "passing_tone_rule: FALSE, non-harmonic interval {} disallowed on downbeat".format(ti)))
-        elif timing_i == "U":
+        if timing_i == 0.:
+            returns.append((None, "passing_tone_rule: NONE, rule not applicable on beat {}".format(timing_i)))
+        elif timing_i != 0.:
             if ti in harmonic_intervals or ti in neg_harmonic_intervals:
                 returns.append((True, "passing_tone_rule: TRUE, harmonic interval {} allowed on downbeat".format(ti)))
             else:
@@ -1964,8 +1991,119 @@ def passing_tone_rule(parts, durations, key_signature, time_signature, mode, tim
             raise ValueError("passing_tone_rule: shouldn't get here")
     return returns
 
+
+def sequence_step_rule(parts, durations, key_signature, time_signature, mode, timings, ignore_voices):
+    rules = rules_from_midi(parts, durations, key_signature)
+    rules = rules[0]
+    key = key_signature_inv_map[key_signature]
+    returns = []
+    assert all([len(timings[i]) == len(timings[0]) for i in range(len(timings))])
+    last_timing_i = 0.
+    for idx, rule in enumerate(rules):
+        last, this = rsp(rule)
+        tm, ti, tn = this.split(":")
+        tn0, tn1 = tn.split(",")
+
+        timing_i = timings[0][idx]
+        for n in range(len(timings)):
+            assert timings[n][idx] == timing_i
+
+        time_num = time_signature[0]
+        time_denom = time_signature[1]
+
+        diff_timing_i = timing_i - last_timing_i
+        # diff timing is circular
+        if timing_i == 0. and last_timing_i == 3.:
+            diff_timing_i = 1.
+        last_timing_i = timing_i
+        # force to match quarters
+        if timing_i not in [0., 1., 2., 3.]:
+            raise ValueError("sequence_step_rule: timing not recognized!")
+        if idx < 1 or abs(diff_timing_i) != 1.:
+            returns.append((None, "sequence_step_rule: NONE, not applicable at step {}".format(idx)))
+            continue
+        elif abs(diff_timing_i) == 1.:
+            lm, li, ln = last.split(":")
+            ln0, ln1 = ln.split(",")
+
+            pitches = np.array(notes_to_midi([[ln0, ln1], [tn0, tn1]]))
+            last_diffs = np.diff(pitches, axis=0)
+            not_skip = [n for n in range(last_diffs.shape[1]) if n not in ignore_voices]
+            last_diffs = last_diffs[:, not_skip]
+            last_ok = np.where(np.abs(last_diffs) >= 3)[0]
+
+            if idx + 1 == len(rules):
+                if ti in harmonic_intervals or ti in neg_harmonic_intervals:
+                    returns.append((True, "sequence_step_rule: TRUE, interval {} always allowed".format(ti)))
+                elif len(last_ok) == 0 and timing_i not in [0., 2.]:
+                    returns.append((True, "sequence_step_rule: TRUE, interval {} is a continuation".format(ti)))
+                else:
+                    returns.append((False, "sequence_step_rule: FALSE, interval {} disallowed in termination".format(ti)))
+                continue
+
+            this, nxt = rsp(rules[idx + 1])
+            nm, ni, nn = nxt.split(":")
+            nn0, nn1 = nn.split(",")
+            pitches = np.array(notes_to_midi([[tn0, tn1], [nn0, nn1]]))
+            nxt_diffs = np.diff(pitches, axis=0)
+            nxt_diffs = nxt_diffs[:, not_skip]
+            nxt_ok = np.where(np.abs(nxt_diffs) >= 3)[0]
+
+            if ti in harmonic_intervals or ti in neg_harmonic_intervals:
+                returns.append((True, "sequence_step_rule: TRUE, interval {} always allowed".format(ti)))
+            else:
+                if timing_i == 0.:
+                    returns.append((False, "sequence_step_rule: FALSE, cannot have non-harmonic interval {} on bar part 0.".format(ti)))
+                elif timing_i == 1.:
+                    if len(nxt_ok) == 0 and len(last_ok) == 0:
+                        if ni in harmonic_intervals or ni in neg_harmonic_intervals:
+                            returns.append((True, "sequence_step_rule: TRUE, interval {} at bar part 1. allowed as part of continuation".format(ti)))
+                        else:
+                            returns.append((False, "sequence_step_rule: FALSE, interval {} at bar part 1. not allowed, next interval not harmonic".format(ti)))
+                    else:
+                        nxt, nxtnxt = rsp(rules[idx + 2])
+                        nnm, nni, nnn = nxtnxt.split(":")
+                        nnn0, nnn1 = nnn.split(",")
+                        pitches = np.array(notes_to_midi([[nn0, nn1], [nnn0, nnn1]]))
+                        nxtnxt_diffs = np.diff(pitches, axis=0)
+                        nxtnxt_diffs = nxtnxt_diffs[:, not_skip]
+                        nxtnxt_ok = np.where(np.abs(nxtnxt_diffs) >= 3)[0]
+                        nxtnxt_resolves = np.where(np.sign(nxtnxt_diffs) != np.sign(nxt_diffs))[0]
+
+                        # check that it resolves in cabiata...
+                        if len(nxt_ok) == 1 and len(nxtnxt_ok) == 0 and nni in harmonic_intervals and sum(nxtnxt_resolves) == 0:
+                            if not_skip == [1]:
+                                info_tup = (tn1, nn1, nnn1)
+                            elif not_skip == [0]:
+                                info_tup = (tn0, nn0, nnn0)
+                            else:
+                                print("sequence_step_rule: other not_skip voices not yet supported...")
+                                from IPython import embed; embed(); raise ValueError()
+
+                            returns.append((True, "sequence_step_rule: TRUE, cambiata {} -> {} -> {} in voice {} detected at bar part 1.".format(info_tup[0], info_tup[1], info_tup[2], not_skip[0])))
+                        else:
+                            returns.append((False, "sequence_step_rule: FALSE, interval {} at bar part 1. not allowed, not a continuation or cambiata".format(ti)))
+                elif timing_i == 2.:
+                    returns.append((False, "sequence_step_rule: FALSE, cannot have non-harmonic interval {} on bar part 2.".format(ti)))
+                elif timing_i == 3.:
+                    if len(nxt_ok) == 0 and len(last_ok) == 0:
+                        if ni in harmonic_intervals or ni in neg_harmonic_intervals:
+                            returns.append((True, "sequence_step_rule: TRUE, interval {} at bar part 3. allowed as part of continuation".format(ti)))
+                        else:
+                            returns.append((False, "sequence_step_rule: FALSE, interval {} at bar part 3. not allowed, next interval not harmonic".format(ti)))
+                    else:
+                        returns.append((False, "sequence_step_rule: FALSE, interval {} at bar part 3. not allowed, not a continuation".format(ti)))
+                else:
+                    print("sequence_step_rule: shouldn't get here")
+                    from IPython import embed; embed(); raise ValueError()
+        else:
+            print("sequence_step_rule, shouldn't get here")
+            from IPython import embed; embed(); raise ValueError()
+    return returns
+
 species1_rules_map = OrderedDict()
 species1_rules_map["key_start_rule"] = key_start_rule
+species1_rules_map["bar_consonance_rule"] = bar_consonance_rule
 species1_rules_map["next_step_rule"] = next_step_rule
 species1_rules_map["parallel_rule"] = parallel_rule
 
@@ -1986,6 +2124,7 @@ def check_species1_rule(parts, durations, key_signature, time_signature, mode, t
 
 species2_rules_map = OrderedDict()
 species2_rules_map["key_start_rule"] = key_start_rule
+species2_rules_map["bar_consonance_rule"] = bar_consonance_rule
 species2_rules_map["parallel_rule"] = parallel_rule
 species2_rules_map["beat_parallel_rule"] = beat_parallel_rule
 species2_rules_map["next_step_rule"] = next_step_rule
@@ -2002,6 +2141,26 @@ def check_species2_rule(parts, durations, key_signature, time_signature, mode, t
             global_check = False
     return (global_check, res)
 
+species3_rules_map = OrderedDict()
+species3_rules_map["key_start_rule"] = key_start_rule
+species3_rules_map["bar_consonance_rule"] = bar_consonance_rule
+species3_rules_map["parallel_rule"] = parallel_rule
+species3_rules_map["beat_parallel_rule"] = beat_parallel_rule
+species3_rules_map["next_step_rule"] = next_step_rule
+species3_rules_map["sequence_step_rule"] = sequence_step_rule
+def check_species3_rule(parts, durations, key_signature, time_signature, mode, timings, ignore_voices):
+    res = [species3_rules_map[arm](parts, durations, key_signature, time_signature, mode, timings, ignore_voices) for arm in species3_rules_map.keys()]
+
+    global_check = True
+    for r in res:
+        rr = [True if ri[0] is True or ri[0] is None else False for ri in r]
+        if all(rr):
+            pass
+        else:
+            global_check = False
+    return (global_check, res)
+
+
 
 def make_timings(durations, beats_per_measure, duration_unit):
     # use normalized_durations?
@@ -2016,17 +2175,20 @@ def make_timings(durations, beats_per_measure, duration_unit):
     all_timings = []
 
     if beats_per_measure == 4 and duration_unit == 1:
-        downbeats = [0.,]
+        pass
+    else:
+        raise ValueError("Beats per measure {} and duration unit {} NYI".format(beats_per_measure, duration_unit))
 
     value_durations = [[float(durations_map[di]) for di in d] for d in durations]
     cumulative_starts = [np.concatenate(([0.], np.cumsum(vd)))[:-1] for vd in value_durations]
     for cline in cumulative_starts:
         this_lines = []
         for cl in cline:
-            if cl % beats_per_measure in downbeats:
-                this_lines.append("D")
-            else:
-                this_lines.append("U")
+            this_lines.append(cl % beats_per_measure)
+            #if cl % beats_per_measure in downbeats:
+            #    this_lines.append("D")
+            #else:
+            #    this_lines.append("U")
         all_lines.append(this_lines)
     return all_lines
 
@@ -2053,7 +2215,6 @@ def analyze_2voices(parts, durations, key_signature_str, time_signature_str, spe
 
     rules = rules_from_midi(parts, durations, key_signature)
     mode = estimate_mode(parts, durations, rules, key_signature)
-    # TODO: should timings be part of beat, rather that "D", "U"?
     timings = estimate_timing(parts, durations, time_signature)
 
     ignore_voices = cantus_firmus_voices
@@ -2061,6 +2222,8 @@ def analyze_2voices(parts, durations, key_signature_str, time_signature_str, spe
         r = check_species1_rule(parts, durations, key_signature, time_signature, mode, timings, ignore_voices)
     elif species == "species2":
         r = check_species2_rule(parts, durations, key_signature, time_signature, mode, timings, ignore_voices)
+    elif species == "species3":
+        r = check_species3_rule(parts, durations, key_signature, time_signature, mode, timings, ignore_voices)
     else:
         raise ValueError("Unknown species argument {}".format(species))
     all_ok = r[0]
@@ -2396,6 +2559,60 @@ def test_species2():
             print("Test passed for note sequence {}".format(fig_name))
 
 
+def test_species3():
+    print("Running test for species3...")
+    all_ex = []
+
+    # fig 55
+    ex = {"notes": [["D3", "E3", "F3", "G3", "A3", "B3", "C4", "D4", "E4", "D4", "B3", "C4", "D4", "C4", "Bb3", "A3", "Bb3", "C4", "D4", "E4", "F4", "F3", "A3", "Bb3", "C4", "A3", "Bb3", "C4", "Bb3", "A3", "G3", "B3", "A3", "D3", "E3", "F3", "G3", "A3", "B3", "C#4", "D4"],
+                    ["D3", "F3", "E3", "D3", "G3", "F3", "A3", "G3", "F3", "E3", "D3"]],
+          "durations": [["1"] * 40 + ["4"], ["4"] * 11],
+          "answers": [True] * 41,
+          "name": "fig55",
+          "cantus_firmus_voice": 1}
+    all_ex.append(ex)
+
+    for ex in all_ex:
+        notes = ex["notes"]
+        durations = ex["durations"]
+        answers = ex["answers"]
+        fig_name = ex["name"]
+        ig = [ex["cantus_firmus_voice"],]
+        parts = notes_to_midi(notes)
+        key_signature = "C"
+        time_signature = "4/4"
+        aok = analyze_2voices(parts, durations, key_signature, time_signature,
+                              species="species3", cantus_firmus_voices=ig)
+        aok_lu = aok[1]
+        aok_rules = aok[2]
+
+        all_answers = [-1] * len(answers)
+
+        for a in aok[-1]:
+            if all_answers[a[0]] == -1:
+                all_answers[a[0]] = a[1]
+            else:
+                if a[1] in [None, True]:
+                    if all_answers[a[0]] == None:
+                        all_answers[a[0]] = True
+                    else:
+                        all_answers[a[0]] &= True
+                else:
+                    if all_answers[a[0]] == None:
+                        all_answers[a[0]] = False
+                    else:
+                        all_answers[a[0]] &= False
+        all_answers = [True if aa == None else aa for aa in all_answers]
+        assert len(all_answers) == len(answers)
+        equal = [aa == a for aa, a in zip(all_answers, answers)]
+        if not all(equal):
+            print("Test FAIL for note sequence {}".format(fig_name))
+        else:
+            print("Test passed for note sequence {}".format(fig_name))
+        from IPython import embed; embed(); raise ValueError()
+
+
+
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description="Demo of utils")
@@ -2404,8 +2621,9 @@ if __name__ == "__main__":
     args = parser.parse_args()
     print_it = args.p
     if not print_it:
-        test_species1()
-        test_species2()
+        #test_species1()
+        #test_species2()
+        test_species3()
     else:
         # fig 5, gradus ad parnassum
         notes = [["A3", "A3", "G3", "A3", "B3", "C4", "C4", "B3", "D4", "C#4", "D4"],
